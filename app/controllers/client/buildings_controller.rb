6474ebd5aebfd1057d31e0b9e5011a9d33c78c08
birdthechicken <sinangundogar61@gmail.com>
2019-05-28 17:03:51 -0500

    edit,index,show,update,destroy.html

--- app/controllers/client/buildings_controller.rb
+++ app/controllers/client/buildings_controller.rb
@@ -1,9 +1,7 @@
 class Client::BuildingsController < ApplicationController
 
   def index
-    response = HTTP.get("http://localhost:3000/api/buildings")
-    @buildings = response.parse
-    render 'index.html.erb'
+    @buildings = Building.all
   end
 
   def new
@@ -11,54 +9,43 @@ class Client::BuildingsController < ApplicationController
   end
 
   def create
-    client_params = {
-                     name: params[:name],
-                     address: params[:address],
-                     height: params[:height],
-                     construction_date: params[:construction_date],
-                     architect: params[:architect]
-                    }
-                    
-    response = HTTP.post(
-                         "http://localhost:3000/api/buildings", 
-                         form: client_params
-                        )
+    @building = Building.create(
+                                 name: params[:name],
+                                 address: params[:address],
+                                 height: params[:height],
+                                 construction_date: params[:construction_date],
+                                 architect: params[:architect],
+                                 image: params[:image]
+                                )
 
-    building = response.parse
-    redirect_to "/client/buildings/#{building['id']}"
+    redirect_to "/client/buildings/#{@building.id}"
   end
 
   def show
-    response = HTTP.get("http://localhost:3000/api/buildings/#{params[:id]}")
-    @building = response.parse
-    render 'show.html.erb'
+    @building = Building.find(params[:id])
   end
 
   def edit
-    response = HTTP.get("http://localhost:3000/api/buildings/#{params[:id]}")
-    @building = response.parse
-    render "edit.html.erb"
+    @building = Building.find(params[:id])
   end
 
   def update
-    client_params = {
+    @building = Building.find(params[:id])
+    @building.update(
                      name: params[:name],
                      address: params[:address],
                      height: params[:height],
                      construction_date: params[:construction_date],
-                     architect: params[:architect]
-                    }
+                     architect: params[:architect],
+                     image: params[:image]
+                    )
 
-    response = HTTP.patch(
-                          "http://localhost:3000/api/buildings/#{params[:id]}", 
-                          form: client_params
-                          )
-
-    redirect_to "/client/buildings/#{params[:id]}"
+    redirect_to "/client/buildings/#{@building.id}"
   end
 
   def destroy
-    response = HTTP.delete("http://localhost:3000/api/buildings/#{params[:id]}")
+    @building = Building.find(params[:id])
+    @building.destroy
     redirect_to "/"
   end
 end
